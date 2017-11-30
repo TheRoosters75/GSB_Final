0d812f36345179628845c8d6970fdb91a5334081
Sam <Sam@192.168.3.8>
2017-11-30 21:18:36 +0100

    suite du dev java

--- application/views/v_AccueilSF.php
+++ application/views/v_AccueilSF.php
@@ -3,31 +3,110 @@
         <title>Gestion des Composants</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="http://localhost/GSB/CSS/CSS.css"> 
+        <link rel="stylesheet" href="Bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <script type="text/javascript" src="<?php echo base_url();?>JS/lesFonctions.js"></script>
         <script type="text/javascript" src="<?php echo base_url();?>JQuery/jquery-3.1.1.js"></script>
         <script type="text/javascript">
-            $(
-                function()
+        
+             $
+           (
+               function()
                 {
-                    getLesComposants();
+                    
+                    
+                    // Au chargement de la page
+                        getLesComposants();
+                        
+                        
+                   }
 
+
+            ),
+          $
+           (
+               function()
+                {
+                    
+                    $('#lstComposants').change(function(){
+                      
+                    $('#codeCompo').val($('#lstComposants').val());
+                    $('#libelleCompo').val($('#lstComposants option:selected').attr('name'));
+                   
+
+
+                    });
                 }
-                
-            );    
+             );
         </script>
     </head>
     
     <body>        
         <h1>Gestion des composants</h1>
         
+         <select id="lstComposants">
+                        <?php 
+                        foreach ($lesComposants as $composant ){
+                        ?> 
+                        <option name="<?php echo $composant->CMP_LIBELLE;?>"
+                                value="<?php echo $composant->CMP_CODE;?>"
+                                > <?php echo $composant->CMP_LIBELLE;?></option>
+                        <?php
+                        }
+                        ?>
+            </select>
         
-        <div id="divComposants"></div>
+        <p>Code composant</p> <input type="text" id="codeCompo">
+        
+        <p>Nom Composant</p> <input type="text" id="libelleCompo">
+        
+        <br> <br>
+        <input id="update" type="button" value="Modifier composant" name="update">
+                 
+           <!-- Fonction de modification dans la BDD /Tableau -->
+           <?php 
+           
+           if($this->input->post('update') !=''  ){
+            $codeCompo = $this->input->post('codeCompo'); 
+            $libelleCompo = $this->input->post('libelleCompo');
+                 
+                   
+                   
+                    $data = array(
+                     'CMP_CODE' => $codeCompo,
+                     'CMP_LIBELLE' =>$libelleCompo,
+                      
+                  );
+                    $this->db->set('CMP_CODE', $codeCompo);
+                    $this->db->set('CMP_LIBELLE', $libelleCompo);
+                    
+                    
+                    $this->db->where('CMP_CODE', $codeCompo);
+                    
+                    $this->db->update('libelleCompo', $data);
+                    
+                    header("refresh: 0;");
+             
+           } 
+           
+           ?>
+        
+        
+        
+        
+        
+        
+        
+            
         <div id="Tableau" align="left">
-        
-        
-    
+        <div class="col-sm-6">
+
+   
        
-    <table cellspacing="2px" cellpadding="2px;" rules="all" style="border:solid 1px black;">
+    <table class="table table-striped" cellspacing="2px" cellpadding="2px;" rules="all" style="border:solid 1px black;">
         <caption>Listes des medicaments</caption>
         <tr> 
             
@@ -52,99 +131,14 @@
             echo "</tr>";
         }
         ?>
-           
+        
+        
+    
     </table> 
-            </div>
-
+            <ul class="pager">
+                <li class="previous"><a href="http://localhost/GSB/index.php">Previous</a></li>
+            </ul>
+        </div>
     </body>
 </html>
 
-<!--
-<!DOCTYPE html>
-
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
-
-<html>
-    <head>
-        <title>Gestions des visiteurs </title>
-        
-        
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         <script type="text/javascript" src="JQuery/jquery-3.1.1.js"></script>
-         <script type="text/javascript" src="JS/lesFonctions.js"></script>
-         <script type="text/javascript">
-         
-             $
-           (
-               function()
-                {
-                    
-                    
-                    // Au chargement de la page
-                        getLesRegions();
-                   
-                   //Quand le user change de region dans la liste
-                   $('#lstRegions').change
-                   (
-                           //
-                    );
-                   
-                   
-                }
-             );
-         
-        </script>
-    </head>
-    <body>
-        <h1>Page d'accueil : Gestion des visiteurs </h1>
-        
-        <div id="divRegions"></div>
-  
-        <p>Code Region </p> <input type="text" disabled="">
-
-        <p>Liebelle Secteur</p> <input type="text">
-        
-        <p>Nom Region</p> <input type="text">
-        
-        <br> <br>
-        <input type="button" value="Modifier regions" id="modifier">
-        <br>  <br>  <br>  <br>  <br>  <br> 
-      
-      
-        <div id="Tableau" align="right">
-    
-       
-    <table cellspacing="2px" cellpadding="2px;" rules="all" style="border:solid 1px black;">
-        <caption>Listes des visiteurs</caption>
-        <tr> 
-            
-              <th>Matricule</th>
-              <th>Nom</th>
-              <th>Prenom</th>
-              <th>Ville</th>
-           
-        </tr>    
-       
-        php 
-        
-        foreach($lesVisiteurs as $visiteur){
-            echo "<tr>";
-           
-            echo"<td> <a href='index.php/Ctrl_AccueilVisiteurAc/getlesvisiteurs'>".$visiteur->VIS_MATRICULE."</a></td>";
-            echo"<td>".$visiteur->VIS_NOM."</td>";
-            echo"<td>".$visiteur->Vis_PRENOM."</td>";
-            echo"<td>".$visiteur->VIS_VILLE."</td>";
-            
-            
-            echo "</tr>";
-        }
-        
-           
-    </table> 
-            </div>
-     
-        </body>
-</html>-->
